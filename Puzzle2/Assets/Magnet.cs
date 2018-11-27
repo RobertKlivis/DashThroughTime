21a2caef5612daf1fe7ab5793d654c66d48565e0
cbevan200 <CBEVAN200@caledonian.ac.uk>
2018-11-27 00:12:03 +0000

    Audio Implemented
    One audio source is now in the scene. However I can't seem to get it running iteratively

--- Puzzle2/Assets/Magnet.cs
+++ Puzzle2/Assets/Magnet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Magnet : MonoBehaviour {
 
     public PointEffector2D pointEffector;
+    public Audio audio;
     bool magnetAffectBridge = false;
 
     // Use this for initialization
@@ -22,9 +23,10 @@ public class Magnet : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             pointEffector.enabled = true;
+            audio.PlayMagnetSound();
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+            if (Input.GetKeyUp(KeyCode.Space))
         {
             pointEffector.enabled = false;
         }
