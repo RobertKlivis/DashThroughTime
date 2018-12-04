5b0d4c93329acfa3fd7f63ae29d29555ef71a490
cbevan200 <CBEVAN200@caledonian.ac.uk>
2018-12-04 12:25:57 +0000

    Switch mechanism implemented
    The code to activate the walls works well. The physics and movement still need adjusted

---
+++ Puzzle2/Assets/Passage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passage : MonoBehaviour {
+    public GameObject redWall;
+    public GameObject blueWall;
+	// Use this for initialization
+	void Start () {
+        redWall.SetActive(false);
+        blueWall.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            redWall.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            redWall.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            blueWall.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            blueWall.SetActive(false);
+        }
+	}
+}
